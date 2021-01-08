4dedceb5a03e15abe4a3a5933b8d7d8f94bd25a6
Leila Prelat <leilaprelat@gmail.com>
2021-01-08 16:45:24 -0300

    update 8/2/2021

--- sin_kz_inv/dispersive/real_freq/solve_det_sinkz_vs_mu.py
+++ sin_kz_inv/dispersive/real_freq/solve_det_sinkz_vs_mu.py
@@ -58,14 +58,15 @@ except ModuleNotFoundError:
 
 print('Definir parametros del problema')
 
-R = 0.3             #micrones
+R = 0.5             #micrones
 list_modos = [1,2,3,4]
 # list_modos = [4]
 
-Ep = 0.7                               
+# Ep = 0.7                               
 epsiinf_DL = 3.9
 gamma_DL = 0.01 #unidades de energia
 
+list_Ep = [0.7,0.4,0.5,0.3,0.8,0.6,0.9]
 list_mu =  np.linspace(0.9,0.3,6001)  
 # list_mu = [0.3]
 
@@ -82,26 +83,22 @@ if save_data_opt==1:
 
     path_save0 = r'/R_%.2f/epsiinf_DL_%.2f_vs_mu' %(R,epsiinf_DL)
     path0 = path_basic + path_save0
-    path = path0 + '/' + 'Ep_%.1f' %(Ep)
     
     if not os.path.exists(path0):
         print('Creating folder to save data')
         os.mkdir(path0)
-    
-    if not os.path.exists(path):
-        print('Creating folder to save data')
-        os.mkdir(path)
 
 #%%
 
 print('Definir las condiciones iniciales para el metodo de minimizacion: usar las funciones de QE_lossless.py')
 
-def fcond_inicial(hbaramu,modo):
+def fcond_inicial(hbaramu,modo,Ep):
     """
     Parameters
     ----------
     hbaramu : potencial quimico del grafeno en eV
     modo : modo entero
+    Ep : plasma energy [eV]
 
     Returns
     -------
@@ -126,129 +123,138 @@ label_QE = 'QE approx sin perdidas'
 labelx = '$\mu_c$ [eV]'
 labely = r'$\epsilon_{ci}$'
 
-for modo in list_modos:
-    cond_inicial = fcond_inicial(mu0,modo)
+for Ep in list_Ep:
     
-    info1 = 'R = %.2f $\mu$m, $E_p$ = %.3f eV' %(R,Ep)
-    info2 = 'modo = %i, $\epsilon_\infty$ = %.1f, $\gamma_{DL}$ = %.2f eV' %(modo,epsiinf_DL,gamma_DL)
-    title = info1 +'\n' + info2  + ', ' + name_this_py
-    title2 = 'Sin kz invibilidad' + ', ' + title
-    info = info1 + ', ' + info2
+    path = path0 + '/' + 'Ep_%.1f' %(Ep)
     
-    epsi1_imag_opt = []
-    omegac_opt = []
-    eq_det = []
-    list_mu_opt = []
-    
-
-    
-    for mu in list_mu:
-        mu = np.round(mu,4)
-        print('')
-        print(mu)
-    
-        def det_2variables(x):
-            [omegac,epsi_ci] = x
-            rta = determinante(omegac,Ep,epsiinf_DL,gamma_DL,epsi_ci,modo,R,mu)
-            return np.abs(rta)
-            
-        res = minimize(det_2variables, cond_inicial, method='Nelder-Mead', tol=tol_NM, 
-                       options={'maxiter':ite_NM})
-    #        print(res.message)
-        if res.message == 'Optimization terminated successfully.':
-        # a = omegac_cuasi(modo,Ep,epsiinf_DL,gamma_DL,R,mu)
-            value = det_2variables([res.x[0],res.x[1] ])
-        # if res.x[1] <=  im_epsi1_cuasi(a,Ep,epsiinf_DL,gamma_DL,modo,R,mu) and value < tol_NM : #QE tiene que requerir menor medio activo que la sol numerica
-            omegac_opt.append(res.x[0])
-            epsi1_imag_opt.append(res.x[1])
-            eq_det.append(value)
-            list_mu_opt.append(mu)
-            
-            cond_inicial = [res.x[0],res.x[1]]
-        
-            
     if save_data_opt==1:
-        os.chdir(path)
-        print('Guardar data de minimizacion en .txt')
-    
-        tabla = np.array([list_mu_opt,omegac_opt,epsi1_imag_opt,eq_det])
-        tabla = np.transpose(tabla)
-        info2 = '.Opt det SIN kz nano invisibilidad, ' + info
-        header1 = 'mu [eV]     Omega/c [1/micrones]    Im(epsi1)     Eq(det)' + info2 + ', ' + name_this_py
-        np.savetxt('opt_det_sinkz_inv_vs_mu_modo%i.txt' %(modo), tabla, fmt='%1.9e', delimiter='\t', header = header1)
-    
-
-    
-    im_epsi1_QE = []
-    omegac_QE = []
-    for mu in list_mu:
-        a = omegac_cuasi(modo,Ep,epsiinf_DL,gamma_DL,R,mu)
-        b = im_epsi1_cuasi(a,Ep,epsiinf_DL,gamma_DL,modo,R,mu) 
-
-        im_epsi1_QE.append(b)
-        omegac_QE.append(a)
-    
-
-    # fig = plt.figure(figsize=tamfig)
-    # ax1 = fig.add_subplot(211)
-    # ax2 = fig.add_subplot(212, sharex = ax1)
-    # fig.subplots_adjust(hspace=0.05)
-    # ax1.set_title(title,loc = 'center')
-    # ax1.title.set_size(tamtitle)
-    # ax1.plot(list_mu,im_epsi1_QE,'.m',ms=10,label=label_QE)
-    # ax2.plot(list_mu_opt,epsi1_imag_opt,'.-r',ms=10,label=label_graph)
-    # plt.setp(ax2.get_xticklabels(), fontsize = tamnum)
-    # plt.setp(ax2.get_yticklabels(), fontsize = tamnum)
-    # plt.setp(ax1.get_xticklabels(), visible = False)
-    # plt.setp(ax1.get_yticklabels(), fontsize = tamnum)
-    
-    # ax1.set_ylabel(labely,fontsize=tamletra)
-    # ax2.set_xlabel(labelx,fontsize=tamletra)
-    # ax2.set_ylabel(labely,fontsize=tamletra)
-    
-    # ax1.legend(loc='best',markerscale=2,fontsize=tamlegend)
-    # ax2.legend(loc='best',markerscale=2,fontsize=tamlegend)
-    # ax1.grid(1)      
-    # ax2.grid(1)  
-    # if save_graphs==1:
-    #     os.chdir(path)
-    #     plt.savefig('Im_epsi1_vs_mu_%i'%(modo))
-    
-    plt.figure(figsize=tamfig)
-    plt.plot(list_mu,im_epsi1_QE,'.m',ms=10,label=label_QE)
-    plt.plot(list_mu_opt,epsi1_imag_opt,'.-r',ms=10,label=label_graph)
-    plt.title(title2,fontsize=tamtitle)
-    plt.ylabel(labely,fontsize=tamletra)
-    plt.xlabel(labelx,fontsize=tamletra)
-    plt.tick_params(labelsize = tamnum)
-    plt.legend(loc='best',markerscale=2,fontsize=tamlegend)
-    plt.grid(1)
-    if save_graphs==1:
-        os.chdir(path)
-        plt.savefig('Im_epsi1_vs_mu_%i'%(modo))
+        if not os.path.exists(path):
+            print('Creating folder to save data')
+            os.mkdir(path)
         
-    plt.figure(figsize=tamfig)
-    plt.plot(list_mu,omegac_QE,'.m',ms=10,label=label_QE)
-    plt.plot(list_mu_opt,omegac_opt,'.-r',ms=10,label=label_graph)
-    plt.title(title2,fontsize=tamtitle)
-    plt.ylabel(r'$\omega/c$ [1/$\mu$m]',fontsize=tamletra)
-    plt.xlabel(labelx,fontsize=tamletra)
-    plt.tick_params(labelsize = tamnum)
-    plt.legend(loc='best',markerscale=2,fontsize=tamlegend)
-    plt.grid(1)
-    if save_graphs==1:
-        plt.savefig('Omegac_vs_mu_%i'%(modo))
-
-
-    plt.figure(figsize=tamfig)
-    plt.plot(list_mu_opt,eq_det,'.-r',ms=10,label=label_graph)
-    plt.title(title2,fontsize=tamtitle)
-    plt.ylabel(r'|det|',fontsize=tamletra)
-    plt.xlabel(labelx,fontsize=tamletra)
-    plt.tick_params(labelsize = tamnum)
-    plt.legend(loc='best',markerscale=2,fontsize=tamlegend)
-    plt.grid(1)
-    if save_graphs==1:
-        plt.savefig('det_vs_mu_%i'%(modo))
+    for modo in list_modos:
+        cond_inicial = fcond_inicial(mu0,modo,Ep)
+        
+        info1 = 'R = %.2f $\mu$m, $E_p$ = %.3f eV' %(R,Ep)
+        info2 = 'modo = %i, $\epsilon_\infty$ = %.1f, $\gamma_{DL}$ = %.2f eV' %(modo,epsiinf_DL,gamma_DL)
+        title = info1 +'\n' + info2  + ', ' + name_this_py
+        title2 = 'Sin kz invibilidad' + ', ' + title
+        info = info1 + ', ' + info2
+        
+        epsi1_imag_opt = []
+        omegac_opt = []
+        eq_det = []
+        list_mu_opt = []
+        
+    
+        
+        for mu in list_mu:
+            mu = np.round(mu,4)
+            print('')
+            print(mu)
+        
+            def det_2variables(x):
+                [omegac,epsi_ci] = x
+                rta = determinante(omegac,Ep,epsiinf_DL,gamma_DL,epsi_ci,modo,R,mu)
+                return np.abs(rta)
+                
+            res = minimize(det_2variables, cond_inicial, method='Nelder-Mead', tol=tol_NM, 
+                           options={'maxiter':ite_NM})
+        #        print(res.message)
+            if res.message == 'Optimization terminated successfully.':
+            # a = omegac_cuasi(modo,Ep,epsiinf_DL,gamma_DL,R,mu)
+                value = det_2variables([res.x[0],res.x[1] ])
+            # if res.x[1] <=  im_epsi1_cuasi(a,Ep,epsiinf_DL,gamma_DL,modo,R,mu) and value < tol_NM : #QE tiene que requerir menor medio activo que la sol numerica
+                omegac_opt.append(res.x[0])
+                epsi1_imag_opt.append(res.x[1])
+                eq_det.append(value)
+                list_mu_opt.append(mu)
+                
+                cond_inicial = [res.x[0],res.x[1]]
+            
+                
+        if save_data_opt==1:
+            os.chdir(path)
+            print('Guardar data de minimizacion en .txt')
+        
+            tabla = np.array([list_mu_opt,omegac_opt,epsi1_imag_opt,eq_det])
+            tabla = np.transpose(tabla)
+            info2 = '.Opt det SIN kz nano invisibilidad, ' + info
+            header1 = 'mu [eV]     Omega/c [1/micrones]    Im(epsi1)     Eq(det)' + info2 + ', ' + name_this_py
+            np.savetxt('opt_det_sinkz_inv_vs_mu_modo%i.txt' %(modo), tabla, fmt='%1.9e', delimiter='\t', header = header1)
+        
+    
+        
+        im_epsi1_QE = []
+        omegac_QE = []
+        for mu in list_mu:
+            a = omegac_cuasi(modo,Ep,epsiinf_DL,gamma_DL,R,mu)
+            b = im_epsi1_cuasi(a,Ep,epsiinf_DL,gamma_DL,modo,R,mu) 
+    
+            im_epsi1_QE.append(b)
+            omegac_QE.append(a)
+        
+    
+        # fig = plt.figure(figsize=tamfig)
+        # ax1 = fig.add_subplot(211)
+        # ax2 = fig.add_subplot(212, sharex = ax1)
+        # fig.subplots_adjust(hspace=0.05)
+        # ax1.set_title(title,loc = 'center')
+        # ax1.title.set_size(tamtitle)
+        # ax1.plot(list_mu,im_epsi1_QE,'.m',ms=10,label=label_QE)
+        # ax2.plot(list_mu_opt,epsi1_imag_opt,'.-r',ms=10,label=label_graph)
+        # plt.setp(ax2.get_xticklabels(), fontsize = tamnum)
+        # plt.setp(ax2.get_yticklabels(), fontsize = tamnum)
+        # plt.setp(ax1.get_xticklabels(), visible = False)
+        # plt.setp(ax1.get_yticklabels(), fontsize = tamnum)
+        
+        # ax1.set_ylabel(labely,fontsize=tamletra)
+        # ax2.set_xlabel(labelx,fontsize=tamletra)
+        # ax2.set_ylabel(labely,fontsize=tamletra)
+        
+        # ax1.legend(loc='best',markerscale=2,fontsize=tamlegend)
+        # ax2.legend(loc='best',markerscale=2,fontsize=tamlegend)
+        # ax1.grid(1)      
+        # ax2.grid(1)  
+        # if save_graphs==1:
+        #     os.chdir(path)
+        #     plt.savefig('Im_epsi1_vs_mu_%i'%(modo))
+        
+        plt.figure(figsize=tamfig)
+        plt.plot(list_mu,im_epsi1_QE,'.m',ms=10,label=label_QE)
+        plt.plot(list_mu_opt,epsi1_imag_opt,'.-r',ms=10,label=label_graph)
+        plt.title(title2,fontsize=tamtitle)
+        plt.ylabel(labely,fontsize=tamletra)
+        plt.xlabel(labelx,fontsize=tamletra)
+        plt.tick_params(labelsize = tamnum)
+        plt.legend(loc='best',markerscale=2,fontsize=tamlegend)
+        plt.grid(1)
+        if save_graphs==1:
+            os.chdir(path)
+            plt.savefig('Im_epsi1_vs_mu_%i'%(modo))
+            
+        plt.figure(figsize=tamfig)
+        plt.plot(list_mu,omegac_QE,'.m',ms=10,label=label_QE)
+        plt.plot(list_mu_opt,omegac_opt,'.-r',ms=10,label=label_graph)
+        plt.title(title2,fontsize=tamtitle)
+        plt.ylabel(r'$\omega/c$ [1/$\mu$m]',fontsize=tamletra)
+        plt.xlabel(labelx,fontsize=tamletra)
+        plt.tick_params(labelsize = tamnum)
+        plt.legend(loc='best',markerscale=2,fontsize=tamlegend)
+        plt.grid(1)
+        if save_graphs==1:
+            plt.savefig('Omegac_vs_mu_%i'%(modo))
+    
+    
+        plt.figure(figsize=tamfig)
+        plt.plot(list_mu_opt,eq_det,'.-r',ms=10,label=label_graph)
+        plt.title(title2,fontsize=tamtitle)
+        plt.ylabel(r'|det|',fontsize=tamletra)
+        plt.xlabel(labelx,fontsize=tamletra)
+        plt.tick_params(labelsize = tamnum)
+        plt.legend(loc='best',markerscale=2,fontsize=tamlegend)
+        plt.grid(1)
+        if save_graphs==1:
+            plt.savefig('det_vs_mu_%i'%(modo))
 
 #%%
--- sin_kz_inv/non-dispersive/real_freq/critical_values.py
+++ sin_kz_inv/non-dispersive/real_freq/critical_values.py
@@ -6,8 +6,8 @@ Created on Sun Apr 19 13:19:41 2020
 """
 
 """
-Sobre paper.py: graficar optical gain y omega/c obtenidos
-
+Sobre critical_values.py: graficar optical gain y omega/c obtenidos
+                            en formato paper (lindos) y para todos los modos
 """
 
 import numpy as np
@@ -16,10 +16,13 @@ import sys
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import seaborn as sns
+from scipy.interpolate import interp1d # para find_degenerations == 1
+from scipy import optimize
 
 #%%
 
 save_graphs = 1
+find_degenerations = 1 #encontrar degeneraciones en frecuencia: modos con el mismo omega/c
 
 tamfig = (11,7)
 tamlegend = 18
@@ -51,24 +54,24 @@ pi,hb,c,alfac,hbargama,mu1,mu2,epsi2 = constantes()
 
 print('Definir parametros del problema')
 
-re_epsi1 = 4.9
-R = 0.5              #micrones
+re_epsi1 = 3.9
+R = 0.01              #micrones
  
-path_load = path_basic + '/re_epsi1_%.2f_vs_mu' %(re_epsi1)
+path_load = path_basic + '/re_epsi1_%.2f_vs_mu/R_%.2f' %(re_epsi1,R)
 
 info1 = 'R = %.1f $\mu$m, Re($\epsilon_1$) = %.2f' %(R,re_epsi1)
 info2 = '$gamma_c$ = %.4f eV, $\mu_1$ = %i, $\mu_2$ = %i, $\epsilon_2$ = %i' %(hbargama,mu1,mu2,epsi2)
 inf_tot = info1 + ',' + info2 + '. Ver ' + name_this_py
 
 os.chdir(path_load)
-np.savetxt('info_critical_values_nondisp.txt', [inf_tot], fmt='%s')
+np.savetxt('info_critical_values_nondispR_%.2f.txt' %(R), [inf_tot], fmt='%s')
 
 list_color = ['darkred','yellowgreen','steelblue','coral']
 
 #%%
 
-if R != 0.5:
-    raise TypeError('Wrong value for radium')
+# if R != 0.5:
+#     raise TypeError('Wrong value for radium')
 
 #%%
 
@@ -93,7 +96,7 @@ labely = 'Im($\epsilon_1$)'
 k = 0
 for modo in [1,2,3,4]:
     os.chdir(path_load)
-    name = 'opt_det_sinkz_vs_mu_modo%i.txt' %(modo)
+    name = 'opt_det_sinkz_inv_vs_mu_modo%i.txt' %(modo)
     tabla = np.loadtxt(name, delimiter='\t', skiprows=1)
     tabla = np.transpose(tabla)
     [list_mu_opt,omegac_opt,epsi1_imag_opt,eq_det] = tabla
@@ -114,8 +117,8 @@ ticks = [ticks1,ticks2]
 for i in [0,1]:
     axs[i].minorticks_on()
     axs[i].tick_params(labelsize = tamnum)
-    axs[i].set_yticks(ticks[i])
-    axs[i].set_yticklabels(ticks[i])
+    # axs[i].set_yticks(ticks[i])
+    # axs[i].set_yticklabels(ticks[i])
 
 axs[0].set_ylabel(labely,fontsize = tamletra,labelpad=0) 
 axs[1].set_ylabel(labely,fontsize = tamletra,labelpad=6) 
@@ -137,7 +140,7 @@ fig.text(xs,0.422,s2,fontsize = tamletra)
 
 if save_graphs==1:
     os.chdir(path_load)
-    plt.savefig('Im_epsi1_vs_mu')
+    plt.savefig('Im_epsi1_vs_muR_%.2f.png' %(R),format = 'png')
     
 #%% 
 
@@ -148,7 +151,7 @@ plt.figure(figsize=tamfig)
 k = 0
 for modo in [1,2,3,4]:
     os.chdir(path_load)
-    name = 'opt_det_sinkz_vs_mu_modo%i.txt' %(modo)
+    name = 'opt_det_sinkz_inv_vs_mu_modo%i.txt' %(modo)
     tabla = np.loadtxt(name, delimiter='\t', skiprows=1)
     tabla = np.transpose(tabla)
     [list_mu_opt,omegac_opt,epsi1_imag_opt,eq_det] = tabla
@@ -170,7 +173,206 @@ plt.legend(handles=patchList2,loc=[0.02,0.99],ncol=4,fontsize=tamlegend,frameon=
 
 if save_graphs==1:
     os.chdir(path_load)
-    plt.savefig('omegac_vs_mu')
+    plt.savefig('omegac_vs_muR_%.2f.png' %(R),format = 'png')
     
 #%%
  
+if find_degenerations == 1:
+    
+    for modo in [1,2,3]:
+
+        os.chdir(path_load)
+        name = 'opt_det_sinkz_inv_vs_mu_modo%i.txt' %(modo)      
+        tabla = np.loadtxt(name, delimiter='\t', skiprows=1)
+        tabla = np.transpose(tabla)
+        [list_mu_opt,omegac_opt,epsi1_imag_opt,eq_det] = tabla   
+        
+        name2 = 'opt_det_sinkz_inv_vs_mu_modo%i.txt' %(modo + 1)      
+        tabla2 = np.loadtxt(name2, delimiter='\t', skiprows=1)
+        tabla2 = np.transpose(tabla2)
+        [list_mu_opt2,omegac_opt2,epsi1_imag_opt2,eq_det2] = tabla2
+        
+        if list_mu_opt2[0] != list_mu_opt[0]:
+            raise TypeError('Warning: mu del modo', modo, 'distinto del mu del modo', modo + 1)        
+    
+    def salto_modo1(modo):
+        if type(modo) != int:
+            raise TypeError('Modo debe ser entero')
+        if modo < 4:
+            rta = modo + 1
+        elif modo == 4:
+            rta = 1
+        else:
+            raise TypeError('Wrong value for mode: mode <= 4')
+        return rta
+
+    def salto_modo2(modo):
+        if type(modo) != int:
+            raise TypeError('Modo debe ser entero')
+        if modo < 3:
+            rta = modo + 2
+        elif modo == 3:
+            rta = 1
+        elif modo == 4:
+            rta = 2
+        else:
+            raise TypeError('Wrong value for mode: mode <= 4')
+        return rta
+
+    def omega_c(x,modo):
+        """
+        Parameters
+        ----------
+        x : mu_c obtenido para la condicion de spaser
+        modo : mode
+        
+        Returns
+        -------
+        omega/c correspondiente al mu_c y al modo
+        en condicion de spaser 
+        """
+        
+        os.chdir(path_load)
+        name = 'opt_det_sinkz_inv_vs_mu_modo%i.txt' %(modo)      
+        tabla = np.loadtxt(name, delimiter='\t', skiprows=1)
+        tabla = np.transpose(tabla)
+        [list_mu_opt,omegac_opt,epsi1_imag_opt,eq_det] = tabla    
+        
+        f = interp1d(list_mu_opt,omegac_opt)
+        return float(f(x))   
+
+    def im_epsi1_crit(x,modo):
+        """
+        Parameters
+        ----------
+        x : mu_c obtenido para la condicion de spaser
+        modo : mode
+        
+        Returns
+        -------
+        Im(epsi1) correspondiente al mu_c y al modo
+        en condicion de spaser 
+        """
+        
+        os.chdir(path_load)
+        name = 'opt_det_sinkz_inv_vs_mu_modo%i.txt' %(modo)      
+        tabla = np.loadtxt(name, delimiter='\t', skiprows=1)
+        tabla = np.transpose(tabla)
+        [list_mu_opt,omegac_opt,epsi1_imag_opt,eq_det] = tabla    
+        f = interp1d(list_mu_opt,epsi1_imag_opt)
+        return float(f(x)) 
+
+    def resta_f(x,modo): 
+        """
+        Parameters
+        ----------
+        x : mu_c obtenido para la condicion de spaser
+        modo : mode
+        
+        Returns
+        -------
+        minimizar la diferencia entre diferentes omega/c
+        para hallar degeneraciones
+        se usan saltos de a 1 modo
+
+        """
+        if modo > 4:
+            raise TypeError('Wrong value for mode')
+            
+        f1 = omega_c(x,modo)
+        
+        modo2 = salto_modo1(modo)
+        f2 = omega_c(x,modo2)
+        
+        return np.abs(f1 - f2)
+
+
+    def resta_f2(x,modo): 
+        """
+        Parameters
+        ----------
+        x : mu_c obtenido para la condicion de spaser
+        modo : mode
+        
+        Returns
+        -------
+        minimizar la diferencia entre diferentes omega/c
+        para hallar degeneraciones
+        se usan saltos de a 2 modos
+
+        """
+
+        if modo > 4:
+            raise TypeError('Wrong value for mode')
+            
+        f1 = omega_c(x,modo)
+        
+        modo2 = salto_modo2(modo)
+        f2 = omega_c(x,modo2)
+        
+        return np.abs(f1 - f2)
+        
+    list_modo = [1,2,3,4]
+    cond_init = 0.31
+    tol = 1e-9
+    ite = 1150
+    my_dict = {'modes': '','mu_c deg' : '','omega/c deg' : '', 'im_epsi1 del modo 1' : '', 'im_epsi1 del modo 2' : ''}
+    
+    for nu in list_modo :
+        def F(x):
+            return resta_f(x,nu)
+        try:
+            sol = optimize.root(F, cond_init, jac=None, method='hybr', tol=tol, 
+                       options={'maxiter':ite})
+        except ValueError:
+            continue
+        nu2 = salto_modo1(nu)
+        sol1 = np.round(sol.x[0],9)
+        print(nu,nu2, sol1)
+        my_dict['modes'] = nu,nu2
+        my_dict['mu_c deg'] = sol1
+        my_dict['omega/c deg'] = omega_c(sol1,nu)
+        
+        key1_im_epsi1 = 'im_epsi1 del modo 1'
+        my_dict[key1_im_epsi1] = im_epsi1_crit(sol1,nu)
+        my_dict['im_epsi1 del modo %i' %(nu)] = my_dict.pop(key1_im_epsi1)
+        
+        key2_im_epsi1 = 'im_epsi1 del modo 2'
+        my_dict[key2_im_epsi1] = im_epsi1_crit(sol1,nu2)
+        my_dict['im_epsi1 del modo %i' %(nu2)] = my_dict.pop(key2_im_epsi1)
+
+    print('')
+    my_dict2 = {'modes': '','mu_c deg' : '','omega/c deg' : '', 'im_epsi1 del modo 1' : '', 'im_epsi1 del modo 2' : ''}
+    for nu in list_modo :
+        def F(x):
+            return resta_f2(x,nu)
+        try:
+            sol = optimize.root(F, cond_init, jac=None, method='hybr', tol=tol, 
+                       options={'maxiter':ite})
+        except ValueError:
+            continue
+        nu2 = salto_modo2(nu)
+        sol2 = np.round(sol.x[0],9)
+        print(nu, nu2, sol2)    
+        my_dict2['modes'] = nu,nu2
+        my_dict2['mu_c deg'] = sol2
+        my_dict2['omega/c deg'] = omega_c(sol2,nu)
+
+        key1_im_epsi1 = 'im_epsi1 del modo 1'
+        my_dict2[key1_im_epsi1] = im_epsi1_crit(sol2,nu)
+        my_dict2['im_epsi1 del modo %i' %(nu)] = my_dict2.pop(key1_im_epsi1)
+        
+        key2_im_epsi1 = 'im_epsi1 del modo 2'
+        my_dict2[key2_im_epsi1] = im_epsi1_crit(sol2,nu2)
+        my_dict2['im_epsi1 del modo %i' %(nu2)] = my_dict2.pop(key2_im_epsi1)    
+    
+    if my_dict['omega/c deg'] != '' or my_dict2['omega/c deg'] != '' :
+    
+        deg_txt = 'info_degenerations_nondispR_%.2f.txt' %(R)
+        np.savetxt(deg_txt, [], fmt='%s')    
+        with open(deg_txt, 'w+') as file: 
+            file.write('{}\n{}'.format(str(my_dict),str(my_dict2)))
+            # file.writelines([inf_tot,str(my_dict),str(my_dict2)])
+        file.close()
+    
+#%%  
