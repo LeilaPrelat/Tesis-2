631ea0153c9a5a1d2dc0f67d1b1a273a90809c63
Leila Prelat <leilaprelat@gmail.com>
2021-01-14 20:02:22 -0300

    update 14/1/2021

--- coef_an.py
+++ coef_an.py
@@ -37,7 +37,7 @@ pi,hb,c,alfac,hbargama,mu1,mu2,epsi2 = constantes()
 
 #%%
 
-def an(omegac,epsi1,modo,R,hbaramu,Ao): 
+def an_nondisp(omegac,epsi1,modo,R,hbaramu,Ao): 
     """
     Parameters
     ----------
@@ -52,6 +52,7 @@ def an(omegac,epsi1,modo,R,hbaramu,Ao):
 
     Returns
         coeficiente complejo an (el del campo Hz)
+        para el caso no dispersivo 
     -------
     """
     energy = omegac*c*hb
@@ -84,3 +85,64 @@ def an(omegac,epsi1,modo,R,hbaramu,Ao):
     an = an_num/an_den
     
     return an
+
+#%%
+
+def an_disp(omegac,Ep,epsiinf_DL,gamma_DL,epsi_ci,modo,R,hbaramu,Ao): 
+    """
+    Parameters
+    ----------
+    omegac : omega/c en 1/micrometros
+    Ep : hbar*omega_p (Drude-Lorentz) de permeabilidad del medio 1
+    epsiinf_DL: epsilon_infinito (Drude-Lorentz) de permeabilidad del medio 1
+    gamma_DL : gamma (Drude-Lorentz) de permeabilidad del medio 1
+            en unidades eV de energia
+    epsi_ci: Im(epsilon1) del colorante (dye)
+    modo : modo
+    R : radio del cilindro en micrometros
+    hbaramu : potencial quimico del grafeno en eV
+    
+    Ao: pol p ---> Hz (Amplitud del campo incidente en Hz del medio 2)
+
+    Returns
+        coeficiente complejo an (el del campo Hz)
+        para el caso dispersivo
+    -------
+    """
+    energy = omegac*c*hb
+
+    sigmatot, inter, intra = sigma(energy,hbaramu,hbargama) 
+        
+    num = Ep**2
+    den = energy**2 + 1j*gamma_DL*energy
+    epsi1 = epsiinf_DL - num/den + 1j*epsi_ci
+    
+    k0 = omegac
+    Rbarra = R*k0 #adimensional
+    
+    x1,x2 = (epsi1*mu1)**(1/2), (epsi2*mu2)**(1/2)
+        
+    def bessel1(modo):
+        CJ1 = sp.jn(modo,x1*Rbarra)+0j
+        DCJ1 = sp.jvp(modo,x1*Rbarra)+0j 
+        return CJ1,DCJ1
+
+    def bessel2(modo):
+        CJ2 = sp.jn(modo,x2*Rbarra)+0j
+        DCJ2 = sp.jvp(modo,x2*Rbarra)+0j 
+        CH2 = sp.hankel1(modo,x2*Rbarra)+0j
+        DCH2 = sp.h1vp(modo,x2*Rbarra)+0j  
+        return CJ2,DCJ2,CH2,DCH2
+    
+    J1,derJ1 = bessel1(modo)
+    J2,derJ2,H2,derH2 = bessel2(modo)
+    
+    Sigma_ad = 4*pi*1j*sigmatot*alfac #sigma devuelve la conductividad teniendo que multiplicar por alfac*c ---> no hay que dividir por c
+    aux =   (1j)**(modo)
+    an_num = -Ao*aux*(epsi1*x2*J1*derJ2 - epsi2*x1*J2*derJ1 + Sigma_ad*x1*x2*derJ2*derJ1)
+    an_den = epsi1*x2*J1*derH2 - epsi2*x1*H2*derJ1 + Sigma_ad*x1*x2*derH2*derJ1
+    an = an_num/an_den
+    
+    return an
+
+#%%
